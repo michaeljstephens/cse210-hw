286688b5a3a4f60191a7db3727ef32c31b077d3f
michaeljstephens <dynomike524@gmail.com>
2025-02-05 23:42:17 -0700

    w05hw

---
+++ prepare/Learning03/fraction.cs
@@ -0,0 +1,37 @@
+using System;
+//sorry this code looks very similar. I consulted the sample solution
+// quite a bit, especially on part 4.
+public class Fraction
+{
+    private int _top;
+    private int _bottom;
+
+    public Fraction()
+    {
+        _top = 1;
+        _bottom = 1;
+    }
+
+    public Fraction(int wholeNumber)
+    {
+        _top = wholeNumber;
+        _bottom = 1;
+    }
+
+    public Fraction(int top, int bottom)
+    {
+        _top = top;
+        _bottom = bottom;
+    }
+
+    public string GetFractionString()
+    {
+        string text = $"{_top}/{_bottom}";
+        return text;
+    }
+
+    public double GetDecimalValue()
+    {
+        return (double)_top / (double)_bottom;
+    }
+}
